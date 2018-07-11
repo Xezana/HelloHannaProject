9f14acc43637967df9922b518da740d036387da8
Dowlut <hanna.dowlut@accenture.com>
2018-07-11 13:54:07 +0400

    second

---
+++ src/main/java/com/accenture/devops/HelloWorldProjectApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldProjectApplication.class, args);
+	}
+}
